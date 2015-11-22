2513a53da4764572b0ca6658ba4dd292a720f69f
Paul Grave <paul@stomer.net>
2015-11-22 15:03:59 +0000

    removed: everything python. conversion is 100% node now

--- src/jsontoyaml.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import fileinput
-import json
-import yaml
-
-def 
-with open('rules.json', 'r') as f:
-    j = json.load(f)
-    print yaml.safe_dump(j, default_flow_style=False)
