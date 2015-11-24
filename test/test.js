064eaf25bff83990c1eb414ac78df3588d365c0a
Paul Grave <paul@stomer.net>
2015-11-24 06:55:17 +0000

    added: new tests for recent trackers

--- test/test.js
+++ test/test.js
@@ -13,7 +13,10 @@ describe("parse json", function() {
 
 describe('blocked', function() {
   var urls = [
-	  "http://www.lequipe.fr/v6/js/ads-v12.js?2015c"
+	  "http://www.lequipe.fr/v6/js/ads-v12.js?2015c",
+		"http://api.viglink.com/api/ping",
+		"http://bcp.crwdcntrl.net/5/c=7580/rand=289563465/pv=y/int=%23OpR%2367012%23www.cultofmac.com%20%3A%20Total%20Site%20Traffic%20%3A%202/rt=ifr",
+		"http://bh.contextweb.com/bh/rtset?do=add&pid=537085&ev=B780FD9F4F0954567F7E0F0002E53BF8"
   ];
 	var expected = "block";
 		
